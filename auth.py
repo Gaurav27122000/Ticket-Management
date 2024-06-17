c0198188ae2bf9fe61ca12df87aebf3ef772a71e
Gaurav Goyal <gauravgoyal@192.168.1.5>
2024-06-17 16:14:15 +0530

    Increased token expiry time

--- auth.py
+++ auth.py
@@ -101,7 +101,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate user.')
-    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(hours=2))
 
     return {'access_token': token, 'token_type': 'bearer'}
 
