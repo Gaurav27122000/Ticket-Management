fc99aee3529c73d92be0397dadf322647745dcd3
Gaurav Goyal <gauravgoyal@192.168.1.5>
2024-06-17 19:12:05 +0530

    Fixed user role

--- ticket.py
+++ ticket.py
@@ -43,6 +43,7 @@ class TicketStatus(BaseModel):
 
 @router.get("/getTickets")
 async def get_all(user: user_dependency,db: db_dependency):
+    print(user)
     if(user.get('user_role')=='user'):
         return db.query(Ticket).filter(Ticket.owner_id == user.get('id')).all()
     else:
